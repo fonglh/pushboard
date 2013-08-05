70c22b3fe6fba2147dca95d7db229b301999e274
fonglh <fonglh@gmail.com>
2013-08-05 23:24:24 +0800

    Get facebook app id and app secret from the admin's blog and pre-populate it into user created blogs.

--- pushboard.php
+++ pushboard.php
@@ -12,5 +12,14 @@ License: GPL2
 add_action( 'wpmu_new_blog', 'frr_pushboard_init_blog' );
 
 function frr_pushboard_init_blog( $blog_id ) {
-	
+	// get facebook settings from blog 1
+	switch_to_blog(1);
+	$fb_options = get_option( 'facebook_application' );
+
+	// don't use the access token
+	unset( $fb_options[ 'access_token' ] );
+
+	// store the facebook app ID and app secret into the newly created site
+	switch_to_blog( $blog_id );
+	add_option( 'facebook_application', $fb_options );
 }
